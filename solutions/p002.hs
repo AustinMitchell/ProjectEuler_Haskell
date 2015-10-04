208a2c1f436cade8dc312111a9174b4b02da727b
AustinMitchell <bigpapamitchell@gmail.com>
2015-10-03 23:10:24 -0400

    Finished problem #8

--- solutions/p002.hs
+++ solutions/p002.hs
@@ -2,5 +2,5 @@ main = print(sum [x | x <- (genFib [1, 0] 4000000), mod x 2 == 0])
 
 genFib :: [Integer] -> Integer -> [Integer]
 genFib (x:xs) lim 
-	| x + head(xs) <= lim = genFib ((x+head(xs)):x:xs) lim
-	| otherwise = x:xs
+	| x + head(xs) <= lim 	= genFib ((x+head(xs)):x:xs) lim
+	| otherwise 			= x:xs
