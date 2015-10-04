c7bc360911e58feb2be8ec161ae4b025daea3aec
AustinMitchell <bigpapamitchell@gmail.com>
2015-10-03 22:49:25 -0400

    Finished problem #7

---
+++ solutions/p007.hs
@@ -0,0 +1,8 @@
+main = print (primes !! 10000) -- Remember that lists are 0 indexed
+
+-- Faster Eratos prime generation, drawn from http://www.justinshield.com/2011/05/haskell-a-better-prime-number-list-using-sieve-of-eratosthenes-and-lazy-pattern-matching/
+primes :: [Integer]
+primes = 2: 3: sieve (tail primes) [5,7..]  
+ 
+sieve (p:ps) xs = h ++ sieve ps [x | x <- t, rem x p /=0]
+  where (h,~(_:t)) = span(< p*p) xs
